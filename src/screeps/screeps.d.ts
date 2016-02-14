3635f1afe409a6c4a607ceed646a7f2e34a23e8c
Patrick Housley <patrick.f.housley@gmail.com>
2016-02-13 18:36:43 -0600

    Finished the flag interface.

--- src/screeps/screeps.d.ts
+++ src/screeps/screeps.d.ts
@@ -1070,8 +1070,73 @@ declare module Screeps {
     upgradeController(target: Controller): ResultCode;
   }
 
+  /**
+   * A flag. Flags can be used to mark particular spots in a room. Flags are visible to their owners only.
+   */
   interface Flag {
-
+    /**
+     * A unique object identificator. You can use Game.getObjectById method to retrieve an object instance by its id.
+     */
+    id: string;
+    
+    /**
+     * Flag color.
+     */
+    color: Color;
+    
+    /**
+     * A shorthand to Memory.flags[flag.name]. You can use it for quick access the flag's specific memory data object.
+     */
+    memory: Object;
+    
+    /**
+     * Flag’s name. You can choose the name while creating a new flag, and it cannot be changed later. This name is a
+     * hash key to access the spawn via the Game.flags object.
+     */
+    name: string;
+    
+    /**
+     * An object representing the position of this structure in the room.
+     */
+    pos: RoomPosition;
+    
+    /**
+     * The link to the Room object. May not be available in case a flag is placed in a room which you do not have access to.
+     */
+    room: Room;
+    
+    /**
+     * The name of the room in which this flag is in. This property is deprecated and will be removed soon. Use pos.roomName instead.
+     */
+    roomName: string;
+    
+    /**
+     * Remove the flag.
+     * @returns s One of the following codes:
+     * OK => 0 : The operation has been scheduled successfully.
+     */
+    remove(): ResultCode;
+    
+    /**
+     * Set new color of the flag.
+     * @param color One of the color contants.
+     * @returns One of the following codes:
+     * OK => 0 : The operation has been scheduled successfully.
+     * ERR_INVALID_ARGS => -10 : color is not a valid color constant.
+     */
+    setColor(color: Color): ResultCode;
+    
+    /**
+     * Set new position of the flag.
+     * @param x The X position in the room.
+     * @param y The Y position in the room.
+     * @param pos Can be a RoomPosition object or any object containing RoomPosition.
+     * @returns One of the following codes:
+     * OK => 0 : The operation has been scheduled successfully.
+     * ERR_INVALID_TARGET => -7 : The target provided is invalid.
+     */
+    setPosition(x: number, y: number): ResultCode;
+    setPosition(pos: RoomPosition): ResultCode;
   }
 
   interface Map {
